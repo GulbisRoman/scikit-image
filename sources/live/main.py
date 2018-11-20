29e6ef84ab3c6193d8d6607d20f5407dcd7c2ff8
Oleg Dudnik <Oleggelo86@gmail.com>
2018-11-20 20:36:52 +0300

    u

--- sources/live/main.py
+++ sources/live/main.py
@@ -1,6 +1,25 @@
 import numpy as np
 from skimage import io
 import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+def subscale(image, x, y, value, coff):
+	for w in range(coff):
+		for h in range(coff):
+			image[x + w, y + h] = value
+
+def scale(image, coff):
+	shape = image.shape
+	newimage = np.random.randint(0, 2, size=(image.shape[0] * coff, image.shape[1] * coff))
+
+	for i in range(shape[0]):
+		for j in range(shape[1]):
+			subscale(newimage, i * coff, j * coff, image[i, j], coff)
+
+	return newimage
+
 
 def next(x, y, shape):
 	return x - shape[0] if(x >= shape[0]) else x, y - shape[1] if(y >= shape[1]) else y
@@ -11,12 +30,13 @@ def evolve(data):
 	for i in range(shape[0]):
 		for j in range(shape[1]):
 			friendly_neighborhood = field[i - 1, j - 1] + field[i, j - 1] + field[next(i + 1, j - 1, shape)] + field[i - 1, j] + field[next(i + 1, j, shape)] + field[next(i - 1, j + 1, shape)] + field[next(i, j + 1, shape)] + field[next(i + 1, j + 1, shape)]
-			o = field[i, j]
 			if(field[i, j]):
 				field[i, j] = int(friendly_neighborhood == 2 or friendly_neighborhood == 3)
 			else:
 				field[i, j] = int(friendly_neighborhood == 3)
 	return field
+
+
 
 def generateStateOfLive(**kwargs):
 	field = np.random.randint(0, 2, size=kwargs['size'])
@@ -27,12 +47,13 @@ def generateStateOfLive(**kwargs):
 
 	return states
 
-states = generateStateOfLive(size=(25, 25), iterations=10)
+
+# Играем в жизнь
+states = generateStateOfLive(size=(10, 10), iterations=10)
 
 # Сохраняем
-
-if not os.path.exists('kek'):
-	os.makedirs('kek')
+if not os.path.exists('images/kek'):
+	os.makedirs('images/kek')
 
 for i in range(len(states)):
-	io.imsave('kek/{:04d}.png'.format(i + 1), states[i] * 255)
+	io.imsave('images/kek/{:04d}.png'.format(i + 1), scale(states[i] * 255, 50))
