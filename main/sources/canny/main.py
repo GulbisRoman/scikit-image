33faccc557baddd2e29f1aef0d15237cdaa80ebc
Oleg Dudnik <Oleggelo86@gmail.com>
2018-11-21 05:31:28 +0300

    u

--- main/sources/canny/main.py
+++ main/sources/canny/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from skimage import io
 from skimage import feature
 from skimage import color
+from skimage.filters import sobel
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -10,12 +11,18 @@ warnings.filterwarnings("ignore")
 image = io.imread('images/pikachu.png')
 io.imsave('images/pikachu_gray.png', color.rgb2gray(image))
 
+sobel_image = sobel(color.rgb2gray(image))
+io.imsave('images/pikachu_sobel.png', sobel_image)
+
 edges = feature.canny(color.rgb2gray(image)) # default sigma=1.0
 io.imsave('images/pikachu_result.png', edges * 255)
 
 # Самолет
 image = io.imread('images/plane.png')
 io.imsave('images/plane_gray.png', color.rgb2gray(image))
+
+sobel_image = sobel(color.rgb2gray(image))
+io.imsave('images/plane_sobel.png', sobel_image)
 
 edges = feature.canny(color.rgb2gray(image), sigma=4.0)
 io.imsave('images/plane_result.png', edges * 255)
